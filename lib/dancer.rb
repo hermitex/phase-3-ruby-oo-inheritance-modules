9fab3319875134749c6819a1b3a18f1a1ab770f0
hermitex <mercytibs@yahoo.com>
2022-08-24 10:07:06 +0300

    commit tree e27ebf4adaddf2d023efe93a137ff5f392cae3a6

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,15 @@
 require_relative './dance_module'
 require_relative './meta_dancing_module'
+
+class Dancer
+    include Dance
+    
+    attr_accessor :name
+
+    extend MetaDancing
+
+    def initialize(name)
+        @name = name
+    end
+
+end
